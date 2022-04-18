eb9ceed3a9781150cf01b308a74e0ba9e7882884
konok101 <rockykonok@gmail.com>
2022-04-18 17:26:54 +0600

    premative nad non premative

---
+++ Api example/pre.js
@@ -0,0 +1,5 @@
+const x={job: 'web developer'}
+const y=x;
+console.log(x,y);
+x.job='full stack web dev';
+console.log(x,y);
---
+++ premative-nonpremative.js
@@ -0,0 +1,27 @@
+
+/* data types primitive and data types
+
+1. number
+2. string
+3. boolean
+4. undefined
+5. null
+6.symobol
+
+*/
+
+
+
+
+
+/* non premative data types 
+
+7. object
+
+*/
+ 
+const x={job: 'web developer'}
+const y=x;
+console.log(x,y);
+x.job='full stack web dev';
+console.log(x,y);
